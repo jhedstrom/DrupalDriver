77a28335f62079a6e884d5e986ac958a794e3e55
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-10-16 16:20:51 -0700

    More spec tests.

--- src/Drupal/Driver/Exception/Exception.php
+++ src/Drupal/Driver/Exception/Exception.php
@@ -29,7 +29,7 @@ abstract class Exception extends \Exception {
    *
    * @return Session
    */
-  protected function getDriver() {
+  public function getDriver() {
     return $this->driver;
   }
 }
