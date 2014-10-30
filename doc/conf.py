52fb68686a0bdcad57748bc832c27917247d82fe
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-10-30 13:29:28 -0700

    Custom theme.

--- doc/conf.py
+++ doc/conf.py
@@ -12,8 +12,14 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
+import sys, os
+
+# Check if this build is on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+  import sphinx_rtd_theme
+  html_theme = "sphinx_rtd_theme"
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -98,8 +104,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-
+#html_theme = 'default'
+#html_sidebars = {
+#  '**':['globaltoc.html','searchbox.html'],
+#  'using/windows': ['windowssidebar.html', 'searchbox.html'],
+#}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
