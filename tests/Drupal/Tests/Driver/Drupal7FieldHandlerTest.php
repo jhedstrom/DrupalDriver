3f2f784a87660b2aa0433909f6e9cf8d198884ab
Antonio De Marco <antonio@nuvole.org>
2015-03-12 21:08:52 +0100

    Converting array notation style.

--- tests/Drupal/Tests/Driver/Drupal7FieldHandlerTest.php
+++ tests/Drupal/Tests/Driver/Drupal7FieldHandlerTest.php
@@ -32,56 +32,56 @@ class Drupal7FieldHandlerTest extends FieldHandlerAbstractTest {
    */
   public function dataProvider()
   {
-    return [
+    return array(
 
       // Test default text field provided as simple text.
-      [
+      array(
         'DefaultHandler',
-        (object) ['field_text' => 'Text'],
+        (object) array('field_text' => 'Text'),
         'node',
-        ['field_name' => 'field_text'],
-        ['en' => [['value' => 'Text']]],
-      ],
+        array('field_name' => 'field_text'),
+        array('en' => array(array('value' => 'Text'))),
+      ),
 
       // Test default text field provided as array.
-      [
+      array(
         'DefaultHandler',
-        (object) ['field_text' => ['Text']],
+        (object) array('field_text' => array('Text')),
         'node',
-        ['field_name' => 'field_text'],
-        ['en' => [['value' => 'Text']]],
-      ],
+        array('field_name' => 'field_text'),
+        array('en' => array(array('value' => 'Text'))),
+      ),
 
       // Test single-value date field provided as simple text.
-      [
+      array(
         'DatetimeHandler',
-        (object) ['field_date' => '2015-01-01 00:00:00'],
+        (object) array('field_date' => '2015-01-01 00:00:00'),
         'node',
-        ['field_name' => 'field_date'],
-        ['en' => [['value' => '2015-01-01 00:00:00']]],
-      ],
+        array('field_name' => 'field_date'),
+        array('en' => array(array('value' => '2015-01-01 00:00:00'))),
+      ),
 
       // Test single-value date field provided as an array.
-      [
+      array(
         'DatetimeHandler',
-        (object) ['field_date' => ['2015-01-01 00:00:00']],
+        (object) array('field_date' => array('2015-01-01 00:00:00')),
         'node',
-        ['field_name' => 'field_date'],
-        ['en' => [['value' => '2015-01-01 00:00:00']]],
-      ],
+        array('field_name' => 'field_date'),
+        array('en' => array(array('value' => '2015-01-01 00:00:00'))),
+      ),
 
       // Test double-value date field. Can only be provided as an array
       // due to array type casting we perform in
       // \Drupal\Driver\Fields\Drupal7\AbstractFieldHandler::__call()
-      [
+      array(
         'DatetimeHandler',
-        (object) ['field_date' => [['2015-01-01 00:00:00', '2015-01-02 00:00:00']]],
+        (object) array('field_date' => array(array('2015-01-01 00:00:00', '2015-01-02 00:00:00'))),
         'node',
-        ['field_name' => 'field_date', 'columns' => ['value' => '', 'value2' => '']],
-        ['en' => [['value' => '2015-01-01 00:00:00', 'value2' => '2015-01-02 00:00:00']]],
-      ],
+        array('field_name' => 'field_date', 'columns' => array('value' => '', 'value2' => '')),
+        array('en' => array(array('value' => '2015-01-01 00:00:00', 'value2' => '2015-01-02 00:00:00'))),
+      ),
 
-    ];
+    );
   }
 
 }
