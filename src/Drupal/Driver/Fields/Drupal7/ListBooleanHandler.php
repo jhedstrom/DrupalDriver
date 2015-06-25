bbd844d03376fec308375802fbdbc847ba55e68a
Ema Panz <thePanz@users.noreply.github.com>
2015-06-25 13:32:37 +0200

    Updated code comments

--- src/Drupal/Driver/Fields/Drupal7/ListBooleanHandler.php
+++ src/Drupal/Driver/Fields/Drupal7/ListBooleanHandler.php
@@ -10,7 +10,7 @@ namespace Drupal\Driver\Fields\Drupal7;
 /**
  * ListBoolean field handler for Drupal 7.
  *
- * Nothing to do here, standard ListTextHandler methods are used.
+ * Nothing to do here, the expand() method from ListTextHandler is used.
  */
 class ListBooleanHandler extends ListTextHandler {
 
