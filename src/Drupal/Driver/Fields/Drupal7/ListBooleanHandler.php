d162e1ec7df8e6044f2a378432dfd6f42b70e1a5
Ema Panz <thePanz@users.noreply.github.com>
2015-06-25 10:50:45 +0200

    Add list_boolean field handler
    The list_boolean field type (defined in List core module) is not correctly handled in current implementation: this handler forces the field type to be correctly handled as a "list"

---
+++ src/Drupal/Driver/Fields/Drupal7/ListBooleanHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Driver\Fields\Drupal7\ListBooleanHandler
+ */
+
+namespace Drupal\Driver\Fields\Drupal7;
+
+/**
+ * Class ListBooleanHandler
+ * @package Drupal\Driver\Fields\Drupal7
+ */
+class ListBooleanHandler extends ListTextHandler {
+    // Nothing to do here, standard ListTextHandler methods are used.
+}
