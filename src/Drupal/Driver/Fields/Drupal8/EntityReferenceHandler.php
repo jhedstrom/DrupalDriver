cc91d01d5ff6a6c139431ee133f8c86486b0168e
Will Long <will@kerasai.com>
2015-07-17 11:49:01 -0500

    Implemented bundle check on d8 entity reference field handler

--- src/Drupal/Driver/Fields/Drupal8/EntityReferenceHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/EntityReferenceHandler.php
@@ -19,13 +19,22 @@ class EntityReferenceHandler extends AbstractHandler {
     $return = array();
     $entity_type_id = $this->fieldInfo->getSetting('target_type');
     $entity_definition = \Drupal::entityManager()->getDefinition($entity_type_id);
-    $label = $entity_definition->getKey('label');
+    $label_key = $entity_definition->getKey('label');
+
+    // Determine target bundle restrictions.
+    $settings = $this->fieldConfig->getSettings();
+    if (!empty($settings['handler_settings']['target_bundles'])) {
+      $target_bundle_key = $entity_definition->getKey('bundle');
+      $target_bundles = $settings['handler_settings']['target_bundles'];
+    }
+
     foreach ($values as $value) {
-      $entities = \Drupal::entityManager()
-        ->getStorage($entity_type_id)
-        ->loadByProperties(array($label => $value));
-      if ($entities) {
-        $return[] = array_shift($entities)->id();
+      $query = \Drupal::entityQuery($entity_type_id)->condition($label_key, $value);
+      if (isset($target_bundles)) {
+        $query->condition($target_bundle_key, $target_bundles, 'IN');
+      }
+      if ($entities = $query->execute()) {
+        $return[] = array_shift($entities);
       }
       else {
         throw new \Exception(sprintf("No entity '%s' of type '%s' exists.", $value, $entity_type_id));
