8818dba0e35370f9bd2274175977280c52199b6d
Jonathan Hedstrom <jhedstrom@gmail.com>
2015-10-08 09:41:56 -0700

    Revert "provide support for allowed_values_function"

--- src/Drupal/Driver/Fields/Drupal7/ListBooleanHandler.php
+++ src/Drupal/Driver/Fields/Drupal7/ListBooleanHandler.php
@@ -17,26 +17,16 @@ class ListBooleanHandler extends AbstractHandler {
    */
   public function expand($values) {
     $return = array();
-    if (!empty($this->fieldInfo['settings']['allowed_values_function'])) {
-      $allowed_values = $this->fieldInfo['settings']['allowed_values_function']();
-    }
-    else {
-      $allowed_values = $this->fieldInfo['settings']['allowed_values'];
-    }
+    $allowed_values = $this->fieldInfo['settings']['allowed_values'];
     // If values are blank then use keys as value.
     foreach ($allowed_values as $key => $value) {
       if ($value == '') {
         $allowed_values[$key] = $key;
       }
     }
-    $allowed_values_flip = array_flip($allowed_values);
+    $allowed_values = array_flip($allowed_values);
     foreach ($values as $value) {
-      if (isset($allowed_values_flip[$value])) {
-        $return[$this->language][] = array('value' => $allowed_values_flip[$value]);
-      }
-      else {
-        $return[$this->language][] = array('value' => $value);
-      }
+      $return[$this->language][] = array('value' => $allowed_values[$value]);
     }
     return $return;
   }
