a572605c988f2befbb2f2087dbe2089625bcf93d
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-07-22 15:30:57 +1000

    Issue#95: Crude solution for supporting node links.

--- src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
@@ -13,14 +13,29 @@ class LinkHandler extends AbstractHandler {
   public function expand($values) {
     $return = array();
     foreach ($values as $value) {
+      $uri = $value[1];
+
+      // If the uri has no scheme (and is not protocol relative) attempt to find
+      // a node with that label.
+      if (empty($uri_parts['scheme']) && strpos($uri, '//') !== 0) {
+        $entity_type_id = 'node';
+        $query = \Drupal::entityQuery($entity_type_id)->condition('title', $uri);
+        $entities = $query->execute();
+        if (!empty($entities)) {
+          $nid = array_shift($entities);
+          $uri = 'entity:' . $entity_type_id . '/' . $nid;
+        }
+      }
+
       $return[] = array(
         // 'options' is required to be an array, otherwise the utility class
         // Drupal\Core\Utility\UnroutedUrlAssembler::assemble() will complain.
         'options' => array(),
         'title' => $value[0],
-        'uri' => $value[1],
+        'uri' => $uri,
       );
     }
+
     return $return;
   }
 
