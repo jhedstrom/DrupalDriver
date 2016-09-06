fa06576bf00f9b7a3d7e1e14c1b10b650385263c
Brock Fanning <brockfanning@gmail.com>
2016-09-06 11:16:23 -0400

    Two additional date field handlers for D7

---
+++ src/Drupal/Driver/Fields/Drupal7/DateHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\Driver\Fields\Drupal7;
+
+/**
+ * Date field handler for Drupal 7.
+ */
+class DateHandler extends DatetimeHandler {
+}
---
+++ src/Drupal/Driver/Fields/Drupal7/DatestampHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\Driver\Fields\Drupal7;
+
+/**
+ * Datestamp field handler for Drupal 7.
+ */
+class DatestampHandler extends DatetimeHandler {
+}
