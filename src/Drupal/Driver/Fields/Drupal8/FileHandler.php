e80786641085ae04fd76a213646733dd4530362a
William Blanc Dit Jolicoeur <wblancditjolicoeur@clever-age.com>
2017-01-20 16:19:10 +0100

    handles File field expansion for D8 codebase (with coding standards)

--- src/Drupal/Driver/Fields/Drupal8/FileHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/FileHandler.php
@@ -5,5 +5,34 @@ namespace Drupal\Driver\Fields\Drupal8;
 /**
  * File field handler for Drupal 8.
  */
-class FileHandler extends EntityReferenceHandler {
+class FileHandler extends AbstractHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function expand($values) {
+    $data = file_get_contents($values[0]);
+    if (FALSE === $data) {
+      throw new \Exception("Error reading file");
+    }
+
+    /* @var \Drupal\file\FileInterface $file */
+    $file = file_save_data(
+        $data,
+        'public://' . uniqid());
+
+    if (FALSE === $file) {
+      throw new \Exception("Error saving file");
+    }
+
+    $file->save();
+
+    $return = array(
+      'target_id' => $file->id(),
+      'display' => '1',
+      'description' => 'Behat test file',
+    );
+    return $return;
+  }
+
 }
