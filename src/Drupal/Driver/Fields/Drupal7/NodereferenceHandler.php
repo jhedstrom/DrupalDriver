f7cd84e2e56c5b8e08b2c3c81819ffd35c343865
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-11-24 16:37:37 +0000

    Added support for node references

---
+++ src/Drupal/Driver/Fields/Drupal7/NodereferenceHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\EntityReferenceGenerator\Driver\Field;
+
+use Drupal\Driver\Fields\FieldHandlerInterface;
+
+/**
+ * Node reference field handler for Drupal 7.
+ */
+class NodereferenceHandler implements FieldHandlerInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public function expand($values) {
+    $entity_type = 'node';
+    $entity_info = entity_get_info($entity_type);
+    $return = array();
+    foreach ($values as $value) {
+      $nid = db_select($entity_info['base table'], 't')
+        ->fields('t', array($entity_info['entity keys']['id']))
+        ->condition('t.' . $entity_info['entity keys']['label'], $value)
+        ->execute()->fetchField();
+      if ($nid) {
+        $return[$this->language][] = array('nid' => $nid);
+      }
+    }
+
+    return $return;
+  }
+}
