6dbd96eacae87417b6ebb3bc386c2eae6da60774
Jonathan Hedstrom <jhedstrom@gmail.com>
2021-05-07 13:44:37 -0700

    Fix phpcs.

--- src/Drupal/Driver/Fields/Drupal8/DatetimeHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/DatetimeHandler.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Driver\Fields\Drupal8;
 
-use DateTime;
-use DateTimeZone;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 
 /**
@@ -15,8 +13,8 @@ class DatetimeHandler extends AbstractHandler {
    * {@inheritdoc}
    */
   public function expand($values) {
-    $siteTimezone = new DateTimeZone(\Drupal::config('system.date')->get('timezone.default'));
-    $storageTimezone = new DateTimeZone(DateTimeItemInterface::STORAGE_TIMEZONE);
+    $siteTimezone = new \DateTimeZone(\Drupal::config('system.date')->get('timezone.default'));
+    $storageTimezone = new \DateTimeZone(DateTimeItemInterface::STORAGE_TIMEZONE);
     foreach ($values as $key => $value) {
       if (strpos($value, "relative:") !== FALSE) {
         $relative = trim(str_replace('relative:', '', $value));
@@ -28,7 +26,7 @@ class DatetimeHandler extends AbstractHandler {
         // uses UTC for internal storage. If no timezone is specified in a date
         // field value by the step author, assume the default timezone of
         // the Drupal install, and therefore transform it into UTC for storage.
-        $date = new DateTime($value, $siteTimezone);
+        $date = new \DateTime($value, $siteTimezone);
         $date->setTimezone($storageTimezone);
         $values[$key] = $date->format('Y-m-d\TH:i:s');
       }
