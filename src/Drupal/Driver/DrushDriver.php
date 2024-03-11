30326ce8f457c7df89f5617a143e8f5c9251c35c
Dezső BICZÓ <mxr576@users.noreply.github.com>
2024-03-11 11:33:06 +0000

    Add deprecation errors

--- src/Drupal/Driver/DrushDriver.php
+++ src/Drupal/Driver/DrushDriver.php
@@ -95,9 +95,18 @@ class DrushDriver extends BaseDriver {
 
     if ($binary === '') {
       global $_composer_bin_dir;
-      $binary = $_composer_bin_dir . '/drush';
+
+      $binary = $_composer_bin_dir . '/drusha';
+      if (!file_exists($binary)) {
+        trigger_error("Using the Drush Driver without Drush installed at the project level is deprecated in Drupal Driver 2.2.3 and will be removed in 3.0.0. Please ensure Drush is installed on the project level by running 'composer require drush/drush'.", E_USER_DEPRECATED);
+        $binary = 'drush';
+      }
     }
 
+    if ($binary === 'drush') {
+      trigger_error("Relying on the globally installed Drush binary via Drush Launcher is deprecated in Drupal Driver 2.2.3 and will be removed in 3.0.0. Please ensure Drush is installed on the project level by running 'composer require drush/drush'.", E_USER_DEPRECATED);
+    }
+        
     $this->binary = $binary;
 
     if (!isset($random)) {
