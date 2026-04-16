7ddb1555a98f87535f41d9c24883c97d1066a058
Alex Skrypnyk <alex@drevops.com>
2026-04-17 08:03:58 +1000

    Iterated on #237 by @cheppers: added 'OgStandardReferenceHandler' for Organic Groups.
    Co-authored-by: Balazs Ertl-Bakos <3431339+BaluErtl@users.noreply.github.com>

---
+++ src/Drupal/Driver/Fields/Drupal8/OgStandardReferenceHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\Driver\Fields\Drupal8;
+
+/**
+ * Field handler for 'og_standard_reference' (Organic Groups) in Drupal 8.
+ */
+class OgStandardReferenceHandler extends EntityReferenceHandler {
+}
