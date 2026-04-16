421da2c0ad7e7bb0b9588280ede889e08cc593e3
Alex Skrypnyk <alex@drevops.com>
2026-04-16 19:45:27 +1000

    Iterated on #277 by @chrisolof: allowed 'LinkHandler' to accept named field properties.
    Co-authored-by: Christopher O. Caldwell <chrisolof@gmail.com>

--- src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
@@ -13,17 +13,19 @@ class LinkHandler extends AbstractHandler {
   public function expand($values) {
     $return = [];
     foreach ($values as $value) {
+      // Support both named keys (title, uri, options) and numeric indices.
+      $return_value = [
+        'title' => $value['title'] ?? $value[0] ?? NULL,
+        'uri' => $value['uri'] ?? $value[1] ?? NULL,
+        'options' => [],
+      ];
       // 'options' is required to be an array, otherwise the utility class
       // Drupal\Core\Utility\UnroutedUrlAssembler::assemble() will complain.
-      $options = [];
-      if (!empty($value[2])) {
-        parse_str($value[2], $options);
+      $options = $value['options'] ?? $value[2] ?? NULL;
+      if ($options) {
+        parse_str($options, $return_value['options']);
       }
-      $return[] = [
-        'options' => $options,
-        'title' => $value[0],
-        'uri' => $value[1],
-      ];
+      $return[] = $return_value;
     }
     return $return;
   }
